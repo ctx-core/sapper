91a43d60d1fde1ea3f41376b2687d3da06a96e03
Brian Takita <brian.takita@gmail.com>
2020-01-04 19:04:17 -0500

    feat: store: b__ factories

--- store.ts
+++ store.ts
@@ -1,13 +1,20 @@
 import { writable, derived } from 'svelte/store'
 // @ts-ignore
 import { subscribe__debug } from '@ctx-core/store'
-import { clone } from '@ctx-core/object'
+import { _b } from '@ctx-core/object'
 import { _andand } from '@ctx-core/function'
 import { _uuid } from '@ctx-core/uuid'
-export const __page__sapper = writable(null)
-export const __preloading__sapper = writable(null)
-export const __session__sapper = writable(null)
-export const __path__sapper = derived(__page__sapper, _andand('path'))
-export function touch__session(session) {
-	session.update($session=>$session ? clone($session) : _uuid())
-}
+export const b__page__sapper = _b('__page__sapper', ()=>
+	writable(null))
+export const __page__sapper = b__page__sapper()
+export const b__preloading__sapper = _b('__preloading__sapper', ()=>
+	writable(null))
+export const __preloading__sapper = b__preloading__sapper()
+export const b__session__sapper = _b('__session__sapper', ()=>
+	writable(null))
+export const __session__sapper = b__session__sapper()
+export const b__path__sapper = _b('__path__sapper', ctx=>
+	derived(
+		b__page__sapper(ctx),
+		_andand('path')))
+export const __path__sapper = b__path__sapper()
