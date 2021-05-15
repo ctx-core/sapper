3211fd3980e5e2499ec9f44acdff6ca45376d452
Brian Takita <brian.takita@gmail.com>
2021-05-15 10:05:27 -0400

    ## 10.4.0
    ### Minor Changes
    
    - "noImplicitAny": true
    
    ### Patch Changes
    
    - Updated dependencies [undefined]
    - @ctx-core/array@20.3.0
    - @ctx-core/event-log@2.1.0
    - @ctx-core/function@17.9.0
    - @ctx-core/object@17.6.0
    - @ctx-core/store@24.8.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,20 @@
 # @ctx-core/sapper
 
+## 10.4.0
+
+### Minor Changes
+
+- "noImplicitAny": true
+
+### Patch Changes
+
+- Updated dependencies [undefined]
+  - @ctx-core/array@20.3.0
+  - @ctx-core/event-log@2.1.0
+  - @ctx-core/function@17.9.0
+  - @ctx-core/object@17.6.0
+  - @ctx-core/store@24.8.0
+
 ## 10.3.21
 
 ### Patch Changes
--- src/express.ts
+++ src/express.ts
@@ -1,4 +1,5 @@
 import { promisify } from 'util'
+import type { Response } from 'express'
 import { flatten } from '@ctx-core/array'
 const resolve = promisify(require('resolve'))
 import fs from 'fs'
@@ -8,47 +9,47 @@ import fs from 'fs'
 export function _get_asset(opts:_get_asset_opts_T) {
 	const { asset_key, root_dir } = opts
 	return get_asset
-	async function get_asset(_, res) {
+	async function get_asset(_:any, res:Response) {
 		const NODE_ENV = process.env.NODE_ENV
-		const dir__build =
+		const build_dir =
 			(NODE_ENV === 'dev' || NODE_ENV === 'development')
 			? `${root_dir}/__sapper__/dev`
 			: `${root_dir}/__sapper__/build`
-		const path__build = await resolve(`${dir__build}/build.json`)
+		const path__build = await resolve(`${build_dir}/build.json`)
 		const build = JSON.parse(
 			(await fs.promises.readFile(path__build)).toString()
 		)
 		const { assets } = build
-		const str__path__relative = assets[asset_key]
-		const a1__path__relative = flatten([str__path__relative])
+		const relative_path_str = assets[asset_key]
+		const relative_path_a1 = flatten([relative_path_str])
 		res.writeHead(200, {
 			'Content-Type': 'application/javascript',
 		})
 		const body = await _body()
 		res.end(body)
 		async function _body() {
-			const asset_body_a1 = await Promise.all(_a1__promise__asset_body())
+			const asset_body_a1 = await Promise.all(_asset_body_promise_a1())
 			return asset_body_a1.join('\n')
 		}
-		function _a1__promise__asset_body() {
+		function _asset_body_promise_a1() {
 			const promise_a1 = [] as Promise<Buffer>[]
-			for (let i = 0; i < a1__path__relative.length; i++) {
-				promise_a1.push(_asset_body(a1__path__relative[i]))
+			for (let i = 0; i < relative_path_a1.length; i++) {
+				promise_a1.push(_asset_body(relative_path_a1[i]))
 			}
 			return promise_a1
 		}
-		async function _asset_body(path__relative) {
-			const path__resolved =
-				await resolve(`${dir__build}/client/${path__relative}`)
-			return fs.promises.readFile(path__resolved)
+		async function _asset_body(relative_path:string) {
+			const resolved_path =
+				await resolve(`${build_dir}/client/${relative_path}`)
+			return fs.promises.readFile(resolved_path)
 		}
 	}
-}
-export {
-	_get_asset as _get__asset
 }
 export interface _get_asset_opts_T {
 	asset_key:string,
 	root_dir:string,
+}
+export {
+	_get_asset as _get__asset
 }
 export type _get_asset_opts_type = _get_asset_opts_T
