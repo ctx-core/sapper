83d34985a9e84309eab323f8d7726b807785500e
Brian Takita <brian.takita@gmail.com>
2021-07-15 07:41:51 -0400

    ## 14.0.3
    ### Patch Changes
    
    - import: + .js extention
    - Updated dependencies [undefined]
    - @ctx-core/event-log@5.0.3
    
    - fix: page\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\_\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\_b: host\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\$,path\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\$,params\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\$,query\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\$,error\$

--- src/sapper_Ctx.ts
+++ src/sapper_Ctx.ts
@@ -1,4 +1,4 @@
 import type { event_log_Ctx } from '@ctx-core/event-log'
-import type { sapper_ctx_I } from './sapper_ctx_I.generated'
+import type { sapper_ctx_I } from './sapper_ctx_I.generated.js'
 export interface sapper_Ctx extends sapper_ctx_I, event_log_Ctx{
 }
