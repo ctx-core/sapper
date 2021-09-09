bfac93a817924afdc0bf257ad95e2813e322727f
Brian Takita <brian.takita@gmail.com>
2021-09-09 17:18:10 -0400

    update dependencies

--- src/page$_b.ts
+++ src/page$_b.ts
@@ -1,7 +1,7 @@
 import { be_, assign } from '@ctx-core/object'
 import { readable$_set_ctx_, writable$, Writable$ } from '@ctx-core/store'
 import { event_log$_b } from '@ctx-core/event-log'
-import type { page_T, page_host_T, page_path_T, page_params_T, page_query_T, page_error_T } from './page.js'
+import type { page_host_T, page_path_T, page_params_T, page_query_T, page_error_T, PageContext } from './page.js'
 import type { sapper_Ctx } from './sapper_Ctx.js'
 import type { page_host$_T } from './page_host$_b.js'
 import type { page_path$_T } from './page_path$_b.js'
@@ -21,7 +21,7 @@ export const page$_b = be_<sapper_Ctx, typeof key>(key, ctx=>{
 	query$.subscribe(query=>event_log$.add({ query }))
 	const { store: error$, set: set_error } = readable$_set_ctx_<page_error_T|undefined>(undefined)
 	error$.subscribe(error=>event_log$.add({ error }))
-	const page$ = writable$<page_T|null>(null)
+	const page$ = writable$<PageContext|null>(null)
 	page$.subscribe(page=>{
 		event_log$.add({ page })
 		set_host(page?.host)
@@ -34,7 +34,7 @@ export const page$_b = be_<sapper_Ctx, typeof key>(key, ctx=>{
 		host$, path$, params$, query$, error$,
 	}) as page$_T
 })
-export type page$_T = Writable$<page_T>&{
+export interface page$_T extends Writable$<PageContext> {
 	host$:page_host$_T
 	path$:page_path$_T
 	params$:page_params$_T
