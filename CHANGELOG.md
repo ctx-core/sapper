3b9c4ce28f921d352ae0cc9b59a243f085d690aa
Brian Takita <brian.takita@gmail.com>
2023-12-10 08:36:53 -0500

    - @ctx-core/function: ^21.23.2 -> ^21.23.3 - @ctx-core/object: ^31.1.22 -> ^31.1.23 - @ctx-core/array: ^27.1.2 -> ^27.1.3 - Updated dependencies - Updated dependencies - Updated dependencies - Updated dependencies - Updated dependencies - @ctx-core/env@17.4.46 - @ctx-core/svelte@15.0.27 - @ctx-core/event-log@9.2.46

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,21 @@
 # @ctx-core/sapper
 
+## 21.2.51
+
+### Patch Changes
+
+- @ctx-core/function: ^21.23.2 -> ^21.23.3
+- @ctx-core/object: ^31.1.22 -> ^31.1.23
+- @ctx-core/array: ^27.1.2 -> ^27.1.3
+- Updated dependencies
+- Updated dependencies
+- Updated dependencies
+- Updated dependencies
+- Updated dependencies
+  - @ctx-core/env@17.4.46
+  - @ctx-core/svelte@15.0.27
+  - @ctx-core/event-log@9.2.46
+
 ## 21.2.50
 
 ### Patch Changes
--- page/index.js
+++ page/index.js
@@ -1,51 +1,56 @@
-import { be_, globalThis__be_ } from '@ctx-core/object'
+import { globalThis__be_ } from '@ctx-core/object'
 import { be_readable_pair_, be_writable_triple_, derived_, writable_ } from '@ctx-core/svelte'
 export const [
 	page$_,
 	page_,
 	page__set,
-] = be_writable_triple_(globalThis__be_(
-	()=>writable_(null),
-	{ id: 'page' }))
+] = be_writable_triple_(
+	globalThis__be_('page', ()=>
+		writable_(null)))
 export { page$_ as page__ }
 export const [
 	page__host$_,
 	page__host_,
-] = be_readable_pair_(globalThis__be_(ctx=>
-	derived_(page$_(ctx),
-		page=>
-			page ? page.host : null
-	), { id: 'page__host' }))
+] = be_readable_pair_(
+	globalThis__be_('page__host', ctx=>
+		derived_(page$_(ctx),
+			page=>
+				page ? page.host : null
+		)))
 export { page__host$_ as page__host__ }
 export const [
 	page__path$_,
 	page__path_,
-] = be_readable_pair_(globalThis__be_(ctx=>
-	derived_(page$_(ctx), page=>
-		page ? page.path : null
-	), { id: 'page__path' }))
+] = be_readable_pair_(
+	globalThis__be_('page__path', ctx=>
+		derived_(page$_(ctx), page=>
+			page ? page.path : null
+		)))
 export { page__path$_ as page__path__ }
 export const [
 	page__params$_,
 	page__params_
-] = be_readable_pair_(globalThis__be_(ctx=>
-	derived_(page$_(ctx), page=>
-		page ? page.params : null
-	), { id: 'page__params' }))
+] = be_readable_pair_(
+	globalThis__be_('page__params', ctx=>
+		derived_(page$_(ctx), page=>
+			page ? page.params : null
+		)))
 export { page__params$_ as page__params__ }
 export const [
 	page__query$_,
 	page__query_,
-] = be_readable_pair_(globalThis__be_(ctx=>
-	derived_(page$_(ctx), page=>
-		page ? page.query : null
-	), { id: 'page__query' }))
+] = be_readable_pair_(
+	globalThis__be_('page__query', ctx=>
+		derived_(page$_(ctx), page=>
+			page ? page.query : null
+		)))
 export { page__query$_ as page__query__ }
 export const [
 	page__error$_,
 	page__error_,
-] = be_readable_pair_(globalThis__be_(ctx=>
-	derived_(page$_(ctx), page=>
-		page ? page.error : null
-	), { id: 'page__error', }))
+] = be_readable_pair_(
+	globalThis__be_('page__error', ctx=>
+		derived_(page$_(ctx), page=>
+			page ? page.error : null
+		)))
 export { page__error$_ as page__error__ }
